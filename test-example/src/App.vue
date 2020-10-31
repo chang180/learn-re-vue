5d9fba162258899b488feac492b0441e255fa5af
張建文 <chang180@gmail.com>
2020-10-31 17:01:00 +0800

    update to day15

---
+++ test-example/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    {{ mode }}
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  computed:{
+    mode(){
+      return process.env.NODE_ENV
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
